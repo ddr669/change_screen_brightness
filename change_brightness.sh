dc6a4416a870152c6c8fe193ea8407692e12a0a7
Eduardo Henrique <dudu.herique.x@gmail.com>
2026-02-16 20:17:26 +0000

    calling xrandr before execute to get default monitor screen

--- change_brightness.sh
+++ change_brightness.sh
@@ -9,7 +9,6 @@
 ####				 	 ######
 #####					#######
 
-
 help_me () {
 	echo
 	echo -e -n "**\t**\tchange_brightness.sh\t-__DDr669__-\n"
@@ -17,15 +16,24 @@ help_me () {
 	echo "a bash script to change screen brightness."
 	echo -e -n "Use to to change screenbrightness. \n\n"
 	echo -e -n "Parameters:\n"
-	echo -e -n "\t-b - brightness [ARGS] * A number between 0-10.\n"
-	echo -e -n "\t-h - help\n"
-	echo
+	echo -e -n "\t-b Number - brightness * A number between 0-10.\n"
+	echo -e -n "\t-h - help.\n"
+	
 }
 simple_help () {	
 	echo "[!] Option -b without arguments."
 }
+get_screen (){
+	local screenL=$1
+	echo $screenL	
+}
 new_b=0
+com_ret=$(xrandr --query | grep '\bconnected\b')
+
+_SCREEN=$(get_screen $com_ret)
+echo "[*] Default monitor device: [$_SCREEN] [*]"
 var=$1
+
 if [ ${#var} -eq 0 ]; then
 	help_me
 	exit
@@ -56,10 +64,10 @@ fi
 case $new_b in
 	10)new_b="1.0"
 		echo "[*] Change brightness to $new_b [*]"
-		xrandr --output LVDS-1 --brightness $new_b;;
+		xrandr --output $_SCREEN --brightness $new_b;;
 	0);;
 	*)new_b="0.$new_b"
 		echo "[*] Change brightness to $new_b [*]"
-		xrandr --output LVDS-1 --brightness $new_b;;
+		xrandr --output $_SCREEN --brightness $new_b;;
 esac
 echo
