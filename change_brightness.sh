67a1617252d134c3c59d59f6b8814eacb1d1f030
Eduardo Henrique <dudu.herique.x@gmail.com>
2026-02-16 18:38:04 +0000

    doing some comments.

--- change_brightness.sh
+++ change_brightness.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/sh
+##### Testing in kernel	 ######
+####   Arch1.1  6.18.7	  #####
+###	 github.com/ddr669/ -> ####
+## change_brithness_screen	###
+# -**- Author:__DDr669__ -**-##
+##		date:__/__/__    	###
+###						   ####
+####				 	 ######
+#####					#######
 
 help_me () {
-	#echo -e -n "\t\t"
+	echo
 	echo -e -n "**\t**\tchange_brightness.sh\t-__DDr669__-\n"
 	echo
 	echo "a bash script to change screen brightness."
@@ -13,46 +22,36 @@ help_me () {
 }
 simple_help () {	
 	echo "[!] Option -b without arguments."
-
 }
 new_b=0
 var=$1
-
 if [ ${#var} -eq 0 ]; then
 	help_me
 	exit
 fi
-
 if [ ${#var} -gt 1 ]; then
 	if [ $1 == '-h' ] || [ $1 == '--help' ]; then
 		help_me
 		exit
 	fi
 fi
-
-while getopts b: OPT
-do
+while getopts b: OPT; do
 	case "${OPT}" in
 		b)new_b="${OPTARG}";;
 		*)simple_help
 			break;;
 	esac
 done
-
 if [ $new_b -gt 11 ]; then
 	if [ $new_b -lt 45 ]; then
 		new_b=45
 	fi
 fi
-
 if [ $new_b -lt 4 ] && [ $new_b != 0 ]; then
 	tmp_val=$new_b
-	#new_b=45
 	echo -n "[!]|-The darkness is dangerous "
 	echo "and replace your eyes by the void.-|[!]"
-
 fi
-
 case $new_b in
 	10)new_b="1.0"
 		echo "[*] Change brightness to $new_b [*]"
@@ -62,6 +61,4 @@ case $new_b in
 		echo "[*] Change brightness to $new_b [*]"
 		xrandr --output LVDS-1 --brightness $new_b;;
 esac
-
 echo
-
