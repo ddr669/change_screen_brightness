625aef7b99f126b585e3d52ababda4958b735056
Eduardo Henrique <dudu.herique.x@gmail.com>
2026-02-23 22:25:25 +0000

    change identation to readbility in github

--- change_brightness.sh
+++ change_brightness.sh
@@ -34,25 +34,33 @@ _SCREEN=$(get_screen $com_ret)
 echo "[*] Default monitor device: [$_SCREEN] [*]"
 var=$1
 
-if [ ${#var} -eq 0 ]; then
+if [ ${#var} -eq 0 ];
+then
 	help_me
 	exit
 fi
-if [ ${#var} -gt 1 ]; then
-	if [ $1 == '-h' ] || [ $1 == '--help' ]; then
+if [ ${#var} -gt 1 ];
+then
+	if [ $1 == '-h' ] || [ $1 == '--help' ];
+	then
 		help_me
 		exit
 	fi
 fi
-while getopts b: OPT; do
+while getopts b: OPT
+do
 	case "${OPT}" in
-		b)new_b="${OPTARG}";;
+		b)new_b="${OPTARG}"
+			;;
 		*)simple_help
-			break;;
+			break
+			;;
 	esac
 done
-if [ $new_b -gt 11 ]; then
-	if [ $new_b -lt 45 ]; then
+if [ $new_b -gt 11 ];
+then
+	if [ $new_b -lt 45 ];
+	then
 		new_b=$new_b
 	fi
 fi
@@ -64,10 +72,14 @@ fi
 case $new_b in
 	10)new_b="1.0"
 		echo "[*] Change brightness to $new_b [*]"
-		xrandr --output $_SCREEN --brightness $new_b;;
-	0);;
+		xrandr --output $_SCREEN --brightness $new_b
+		;;
+	0)
+		;;
 	*)new_b="0.$new_b"
 		echo "[*] Change brightness to $new_b [*]"
-		xrandr --output $_SCREEN --brightness $new_b;;
+		xrandr --output $_SCREEN --brightness $new_b
+		;;
 esac
+
 echo
